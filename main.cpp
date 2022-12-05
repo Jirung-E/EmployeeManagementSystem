b2b3fd1dcbaf790c146cd91385a25a1c93da0cb8
Jirung-E <jh10590326@gmail.com>
2022-12-05 18:49:27 +0900

    refactor

--- main.cpp
+++ main.cpp
@@ -1,54 +1,27 @@
-﻿#pragma warning(disable:4996)
-
-#include <iostream>
+﻿#include <iostream>
 #include <locale>
-#include <sstream>
-#include <codecvt>
-#include <fstream>
 
 #include "CSV.h"
 #include "DataReader.h"
 
 using namespace std;
 
-extern wchar_t* cppgetData(const wchar_t* file_path);
-extern void cpploadCSVData(const wchar_t* file_path);
-extern void cppcsvSelectRow(int index);
-extern const wchar_t* cppcsvGetItem(const wchar_t* column);
+
+extern "C" {
+    int test();
+    const wchar_t* getData(const wchar_t* file_path);
+    void loadCSVData(const wchar_t* file_path);
+    void csvSelectRow(int index);
+    const wchar_t* csvGetItem(const wchar_t* column);
+}
 
 
 int main() {
     setlocale(LC_ALL, "");
-    //locale::global(std::locale("kor"));
-    //wcout.imbue(locale("kor"));
 
-    //std::wifstream wif { L"./data.csv" };
-    //wif.imbue(std::locale(std::locale::empty(), new std::codecvt_utf8<wchar_t>));
-    //std::wstringstream wss;
-    //wss << wif.rdbuf();
-    //wcout << wss.str() << endl;
-
-    //wchar_t* ws = cppgetData(L"./data.txt");
-    //wcout << ws << endl;
-
-    //DataReader* reader = new CSVReader;
-    //CSVData data { reader->read(L"./data.csv") };
-
-    //cout << endl;
-
-    //cout << "data[이름]" << endl;
-    //for(const wstring& e : data[L"이름"]) {
-    //    wcout << e << endl;
-    //}
-    //cout << endl;
-
-    //cout << "data[1]" << endl;
-    //for(const wstring& e : data[1]) {
-    //    wcout << e << endl;
-    //}
-
-
-    cpploadCSVData(L"./data.csv");
-    cppcsvSelectRow(1);
-    wcout << cppcsvGetItem(L"이름");
+    cout << test() << endl;
+    wcout << getData(L"./data.txt") << endl;
+    loadCSVData(L"./data.csv");
+    csvSelectRow(2);
+    wcout << csvGetItem(L"계좌") << endl;
 }
